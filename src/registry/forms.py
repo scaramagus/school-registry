8127bd69b075cfb33c9f00575d8f9ac1bb999731
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2016-05-22 23:07:26 -0300

    Fix StudentAdminForm

--- src/registry/forms.py
+++ src/registry/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
+from .models import Student
+
 
 class StudentAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Student
+        fields = ['classroom', 'first_name', 'last_name', 'gender', 'age']
+
     def clean_classroom(self):
         classroom = self.cleaned_data['classroom']
         student_count = classroom.student_set.exclude(
