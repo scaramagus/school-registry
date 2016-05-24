8d345dc7e30cd063641cae3f7b16fec4746792da
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2016-05-23 22:43:57 -0300

    Add templates and static files

--- src/school_registry/settings.py
+++ src/school_registry/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'school_registry.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'registry/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_URL = '/static/'
